b75d446fecbb0ea458d800266fca3eb5f6646404
cheetha <wangzhaochun@gmail.com>
2021-11-18 22:07:14 +0800

    Initial Commit

---
+++ LineWeather/LineWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  LineWeatherApp.swift
+//  LineWeather
+//
+//  Created by ant on 2021/11/18.
+//
+
+import SwiftUI
+
+@main
+struct LineWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
